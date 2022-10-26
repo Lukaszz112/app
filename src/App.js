92f9c0c5ead9d18ad5938f9a9a72ea0549ed4b8d
Lukaszz112 <lendzionn23@gmail.com>
2022-10-26 16:56:04 +0200

    Add bars data, styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <header>My balance $921.48</header>
+        <main>
+          Spending - Last 7 days
+          mon
+          tue
+          wed
+          thu
+          fri
+          sat
+          sun
+
+          Total this month
+          $478.33
+
+          +2.4%
+          from last month
+        </main>
+          
+
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Lukasz</a>.
+
+      </>
+    );
+  }
 }
 
 export default App;
